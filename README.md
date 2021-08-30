013fbcf15fb311928abf017e3ceb47df75f65488
BennyThink <benny.think@gmail.com>
2021-08-30 20:13:30 +0800

    use new wrapper

--- README.md
+++ README.md
@@ -1,50 +1,40 @@
 # NCMBot
+
 🐿️ 能够帮你把网易云音乐ncm格式转换为flac的Telegram Bot
 
 [🐿️ NCMBot](https://t.me/netease_ncm_bot)
 
 # 使用方法
-直接发送文件给bot即可
+
+直接发送文件给bot即可，支持大文件，最大可支持2G
 
 # 截图
+
 ![](assets/1.jpeg)
 
-# 大文件支持
-由于Telegram bot仅支持下载10M文件，因此如果要支持超过10M大文件，就需要用client来解决。
+# 部署
 
 需要去 [Telegram API](my.telegram.org) 申请自己的ID和hash。
 
-# 部署
 ## 普通方式
+
 ```shell script
 git clone https://github.com/tgbot-collection/NCMBot
 cd NCMBot
-# 修改 config.py，或者配置环境变量
 pip3 install -r requirements.txt
-python ncmbot/bot.py
-python ncmbot/client.py
+# 配置环境变量 APP_ID APP_HASH TOKEN
+export APP_ID=1111 APP_HASH=dddd TOKEN=xxx
+python ncmbot.py
 ```
+
 ## docker
 
 ```shell script
-
-# 切换到项目根目录,修改环境配置
-vim config.env 
-# 创建数据库
-touch ncmbot/client.session ncmbot/bot.session
-# 然后进入容器进行基础配置
-docker run --rm -it -v $(pwd)/ncmbot/client.session:/NCMBot/ncmbot/client.session bennythink/ncmbot sh
-# 在容器内运行如下命令
-cd ncmbot
-python client.py
-# 然后输入手机号和验证码，登录client端，Ctrl+C退出即可
-docker-compose up -d
+docker run -d -e APP_ID=1234 -e APP_HASH=1234 -e TOKEN=1NFXI bennythink/ncmbot
 ```
 
-# TODO
-- [x] 支持大文件
-
 # Commands
+
 ```
 start - 开始使用机器人
 about - 关于机器人
@@ -52,4 +42,5 @@ ping - 运行信息
 ```
 
 # License
+
 Apache License 2.0
--- ncmbot.py
+++ ncmbot.py
@@ -7,31 +7,63 @@
 
 __author__ = "Benny <benny.think@gmail.com>"
 
-import tempfile
-import os
-import platform
 import logging
+import os
+import pathlib
+import tempfile
 import traceback
+import typing
 
 import fakeredis
 import filetype
-from telethon import TelegramClient, events, utils, types
-from tgbot_ping import get_runtime
 from ncmdump import dump
-
-from FastTelethon import download_file, upload_file
+from pyrogram import Client, filters, types
+from tgbot_ping import get_runtime
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(filename)s [%(levelname)s]: %(message)s')
 
-app_id = int(os.getenv("APP_ID", 0))
-app_hash = os.getenv("APP_HASH")
-bot = TelegramClient('bot', app_id, app_hash,
-                     device_model=f"{platform.system()} {platform.node()}-{os.path.basename(__file__)}",
-                     system_version=platform.platform()
-                     ).start(bot_token=os.getenv("TOKEN"))
 
+def customize_logger(logger: "list"):
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(filename)s [%(levelname)s]: %(message)s')
+    for log in logger:
+        logging.getLogger(log).setLevel(level=logging.WARNING)
+
+
+def create_app(session="ytdl", workers=20):
+    app_id = int(os.getenv("APP_ID", 0))
+    app_hash = os.getenv("APP_HASH")
+    token = os.getenv("TOKEN")
+
+    _app = Client(session, app_id, app_hash,
+                  bot_token=token, workers=workers)
+
+    return _app
+
+
+customize_logger(["pyrogram.client", "pyrogram.session.session", "pyrogram.client", "pyrogram.connection.connection"])
+app = create_app()
 r = fakeredis.FakeStrictRedis()
 EXPIRE = 5
+
+
+def edit_text(bot_msg, text):
+    key = f"{bot_msg.chat.id}-{bot_msg.message_id}"
+    # if the key exists, we shouldn't send edit message
+    if not r.exists(key):
+        r.set(key, "ok", ex=EXPIRE)
+        bot_msg.edit_text(text)
+
+
+def download_hook(current, total, bot_msg):
+    filesize = sizeof_fmt(total)
+    text = f'[{filesize}]: 下载中 {round(current / total * 100, 2)}% - {current}/{total}'
+    edit_text(bot_msg, text)
+
+
+def upload_hook(current, total, bot_msg):
+    filesize = sizeof_fmt(total)
+    text = f'[{filesize}]: 上传中 {round(current / total * 100, 2)}% - {current}/{total}'
+    edit_text(bot_msg, text)
 
 
 def sizeof_fmt(num: int, suffix='B'):
@@ -42,117 +74,82 @@ def sizeof_fmt(num: int, suffix='B'):
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
-async def download_callback(current, total, chat_id, message):
-    key = f"{chat_id}-{message.id}"
-    # if the key exists, we shouldn't send edit message
-    if not r.exists(key):
-        r.set(key, "ok", ex=EXPIRE)
-        filesize = sizeof_fmt(total)
-        msg = f'[{filesize}]: 下载中 {round(current / total * 100, 2)}% - {current}/{total}'
-        logging.info(msg)
-        new_message = f"{message.text} {msg}"
-        await bot.edit_message(chat_id, message, new_message)
-
-
-async def upload_callback(current, total, chat_id, message):
-    key = f"{chat_id}-{message.id}"
-    # if the key exists, we shouldn't send edit message
-    if not r.exists(key):
-        r.set(key, "ok", ex=EXPIRE)
-        filesize = sizeof_fmt(total)
-        msg = f'[{filesize}]: 上传中 {round(current / total * 100, 2)}% - {current}/{total}'
-        logging.info(msg)
-        new_message = f"{message.text} {msg}"
-        await bot.edit_message(chat_id, message, new_message)
-
-
-def ncm_converter(ncm_path, file_wo_ext) -> dict:
-    logging.info("Converting %s -> %s", ncm_path, file_wo_ext)
+def ncm_converter(tmp_name: "str", normal_name="test.ncm") -> "dict":
+    # normal_name hello.ncm
+    normal_name = normal_name.replace(".ncm", "")
+    tmp_new_name = f"{tmp_name}.temp"
+    logging.info("Converting %s -> %s", tmp_name, tmp_new_name)
     status = {"status": False, "filepath": None, "message": None}
     try:
-        dump(ncm_path, file_wo_ext, False)
-        ext = filetype.guess_extension(file_wo_ext)
-        real_name = file_wo_ext + f".{ext}"
-        os.rename(file_wo_ext, real_name)
+        dump(tmp_name, tmp_new_name, False)
+        ext = filetype.guess_extension(tmp_new_name)
+        real_name = pathlib.PosixPath(tmp_name).parent.joinpath(f"{normal_name}.{ext}").as_posix()
+        os.rename(tmp_new_name, real_name)
         status["status"] = True
         status["filepath"] = real_name
         logging.info("real filename is %s", real_name)
     except Exception:
         err = traceback.format_exc()
-        logging.error("Convert failed for %s -> %s \n%s\n", ncm_path, file_wo_ext, err)
-        status["message"] = err
+        logging.error("Convert failed for %s -> %s \n%s\n", tmp_name, tmp_new_name, err)
+        status["error"] = err
     finally:
         return status
 
 
-@bot.on(events.NewMessage(pattern='/start'))
-async def send_welcome(event):
-    async with bot.action(event.chat_id, 'typing'):
-        await bot.send_message(event.chat_id, "我可以帮你转换网易云音乐的ncm为普通的mp3/flac文件。"
-                                              "直接把ncm文件发给我就可以了。"
-                                              "发送完成之后请耐心等待，如果长时间没回复可以再发一次")
-        raise events.StopPropagation
+@app.on_message(filters.command(["start"]))
+def start_handler(client: "Client", message: "types.Message"):
+    chat_id = message.chat.id
+    client.send_chat_action(chat_id, "typing")
+
+    client.send_message(message.chat.id, "我可以帮你转换网易云音乐的ncm为普通的mp3/flac文件。"
+                                         "直接把ncm文件发给我就可以了。"
+                                         "发送完成之后请耐心等待，如果长时间没回复可以再发一次")
 
 
-@bot.on(events.NewMessage(pattern='/ping'))
-async def send_welcome(event):
-    async with bot.action(event.chat_id, 'typing'):
+@app.on_message(filters.command(["about"]))
+def help_handler(client: "Client", message: "types.Message"):
+    chat_id = message.chat.id
+    client.send_chat_action(chat_id, "typing")
+    client.send_message(chat_id, "网易云ncm格式转换机器人 @BennyThink "
+                                 "GitHub: https://github.com/tgbot-collection/NCMBot")
+
+
+@app.on_message(filters.command(["ping"]))
+def ping_handler(client: "Client", message: "types.Message"):
+    chat_id = message.chat.id
+    client.send_chat_action(chat_id, "typing")
+    if os.uname().sysname == "Darwin":
+        bot_info = "test"
+    else:
         bot_info = get_runtime("botsrunner_ncmbot_1", "NCM Bot")
-        await bot.send_message(event.chat_id, f"{bot_info}\n", parse_mode='md')
-        raise events.StopPropagation
+    client.send_message(chat_id, f"{bot_info}")
 
 
-@bot.on(events.NewMessage(pattern='/about'))
-async def send_welcome(event):
-    async with bot.action(event.chat_id, 'typing'):
-        await bot.send_message(event.chat_id, "网易云ncm格式转换机器人 @BennyThink "
-                                              "GitHub: https://github.com/tgbot-collection/NCMBot")
-        raise events.StopPropagation
-
-
-@bot.on(events.NewMessage(incoming=True, func=lambda e: e.message.file))
-async def echo_all(event):
-    chat_id = event.message.chat_id
-    ncm_name = event.message.file.name
-
-    if event.message.file.ext != ".ncm":
-        await event.reply("不是ncm文件🤔")
+@app.on_message(filters.incoming & filters.document)
+def convert_handler(client: "Client", message: "types.Message"):
+    chat_id = message.chat.id
+    client.send_chat_action(chat_id, "typing")
+    ncm_name = message.document.file_name
+    if not ncm_name.endswith(".ncm"):
+        message.reply("不是ncm文件🤔", quote=True)
         return
 
-    message = await event.reply("文件已收到，正在处理中……")
-
-    temp_dir = tempfile.TemporaryDirectory()
-    file_wo_ext = os.path.join(temp_dir.name, ncm_name[0:-4])
+    bot_message: typing.Union["types.Message", "typing.Any"] = message.reply("文件已收到，正在处理中……", quote=True)
 
     with tempfile.NamedTemporaryFile() as tmp:
-        async with bot.action(event.chat_id, 'audio'):
-            with open(tmp.name, "wb") as out:
-                await download_file(event.client, event.document, out,
-                                    progress_callback=lambda x, y: download_callback(x, y, chat_id, message))
-            await bot.edit_message(chat_id, message, '⏳ 正在转换格式……')
-            result = ncm_converter(out.name, file_wo_ext)
-        if result["status"] is False:
-            async with bot.action(event.chat_id, 'typing'):
-                await bot.edit_message(chat_id, message, f"{ncm_name} 转换失败❌：\n```{result}```",
-                                       parse_mode='markdown')
+        client.send_chat_action(chat_id, "typing")
+        filename = tmp.name
+        message.download(filename, progress=download_hook, progress_args=(bot_message,))
+        bot_message.edit_text("⏳ 正在转换格式……")
+        result = ncm_converter(filename, ncm_name)
+        if result["status"]:
+            client.send_chat_action(chat_id, "upload_audio")
+            client.send_audio(chat_id, result["filepath"],
+                              progress=upload_hook, progress_args=(bot_message,))
+            bot_message.edit_text("转换成功!✅")
         else:
-            real_file = result["filepath"]
-            async with bot.action(event.chat_id, 'document'):
-                with open(real_file, "rb") as out:
-                    res = await upload_file(bot, out,
-                                            progress_callback=lambda x, y: upload_callback(x, y, chat_id, message))
-                attributes, mime_type = utils.get_attributes(real_file)
-                media = types.InputMediaUploadedDocument(
-                    file=res,
-                    mime_type=mime_type,
-                    attributes=attributes,
-                    # not needed for most files, thumb=thumb,
-                    force_file=False
-                )
-                await bot.send_file(chat_id, media)
-                await bot.edit_message(chat_id, message, '转换成功!✅')
-    temp_dir.cleanup()
+            bot_message.edit_text(f"❌转换失败\n\n{result['error'][:4000]}")
 
 
 if __name__ == '__main__':
-    bot.run_until_disconnected()
+    app.run()
